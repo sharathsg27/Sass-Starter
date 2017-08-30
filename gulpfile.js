3088ca656b753857c3d12f92075ec24f647df3cf
sharathsg27 <sharath.sg27@gmail.com>
2017-08-30 12:42:43 +0530

    Master -> Added HTMl layout page with gulp, gulp-saas, lite-server

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp'),
+    saas = require('gulp-sass');
+
+//Create gulp task
+gulp.task('styles', function () {
+    return gulp.src('./sass/styles.scss')
+        .pipe(saas())
+        .pipe(gulp.dest('./css'))
+});
+
+//Watcher
+gulp.task('watch', function () {
+    gulp.watch('./sass/**/*.scss', ['styles'])
+});
+
+gulp.task('default', ['styles']);
